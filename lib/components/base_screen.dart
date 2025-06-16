7c5afbbac065ee02989dfbc0d09254dd7737f94a
Nancy0786 <the.anubhav1404@gmail.com>
2025-06-16 10:34:27 +0000

    Fix: Added content to previously base file

---
+++ lib/components/base_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:world_music_nancy/widgets/background_manager.dart';
+
+class BaseScreen extends StatelessWidget {
+  final Widget child;
+
+  const BaseScreen({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const BackgroundManager(),
+        child,
+      ],
+    );
+  }
+}
