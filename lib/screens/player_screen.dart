9b5625b944ebedd232b76e862fa651630250bcc6
nancy0786 <the.anubhav786@gmail.com>
2025-06-16 23:02:54 +0530

    player_screen.dart

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_music_nancy/components/base_screen.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
 
 class PlayerScreen extends StatefulWidget {
   final String title;
@@ -21,6 +20,7 @@ class PlayerScreen extends StatefulWidget {
 
 class _PlayerScreenState extends State<PlayerScreen> {
   late AudioPlayer _player;
+  bool _isPlaying = false;
 
   @override
   void initState() {
@@ -43,5 +43,54 @@ class _PlayerScreenState extends State<PlayerScreen> {
     super.dispose();
   }
 
+  void _togglePlayback() async {
+    if (_player.playing) {
+      await _player.pause();
+    } else {
+      await _player.play();
+    }
+    setState(() {
+      _isPlaying = _player.playing;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return BaseScreen(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                _isPlaying ? Icons.pause_circle_filled : Icons.play_circle_fill,
+                size: 100,
+                color: Colors.cyanAccent,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                widget.title,
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                widget.author,
+                style: const TextStyle(color: Colors.white70, fontSize: 18),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _togglePlayback,
+                child: Text(_isPlaying ? 'Pause' : 'Play'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
