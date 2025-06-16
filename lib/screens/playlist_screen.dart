63b07e70e6daa698afdd2b89a2700b7d7a9e4100
nancy0786 <the.anubhav786@gmail.com>
2025-06-16 23:05:30 +0530

    playlist_screen.dart

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:world_music_nancy/components/base_screen.dart';
 import 'package:world_music_nancy/providers/playlist_provider.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
-import 'package:world_music_nancy/widgets/playlist_tile.dart'; // <-- ADDED
-import 'package:world_music_nancy/components/base_screen.dart';
+import 'package:world_music_nancy/widgets/playlist_tile.dart';
 import 'package:world_music_nancy/models/playlist.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
-import 'package:world_music_nancy/components/cyberpunk_card.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
 import 'package:world_music_nancy/components/custom_app_bar.dart';
-import 'package:world_music_nancy/components/base_screen.dart';
+
 class PlaylistScreen extends StatelessWidget {
   const PlaylistScreen({super.key});
 
@@ -19,3 +13,25 @@ class PlaylistScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final playlists = context.watch<PlaylistProvider>().playlists;
 
+    return BaseScreen(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: CustomAppBar(title: 'Your Playlists'),
+        body: playlists.isEmpty
+            ? const Center(
+                child: Text(
+                  "No playlists found.",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            : ListView.builder(
+                itemCount: playlists.length,
+                itemBuilder: (context, index) {
+                  final Playlist playlist = playlists[index];
+                  return PlaylistTile(playlist: playlist);
+                },
+              ),
+      ),
+    );
+  }
+}
