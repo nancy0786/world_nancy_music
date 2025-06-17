f314b4dd7bd32a4dac4e5bc00b56027937de398d
nancy0786 <the.anubhav786@gmail.com>
2025-06-17 19:12:04 +0530

    base_screen.dart

--- lib/components/base_screen.dart
+++ lib/components/base_screen.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:world_music_nancy/widgets/background_manager.dart';
 
 class BaseScreen extends StatelessWidget {
-  final Widget child;
   final PreferredSizeWidget? appBar;
+  final Widget child;
   final Widget? floatingActionButton;
   final Widget? bottomNavigationBar;
 
   const BaseScreen({
     super.key,
-    required this.child,
     this.appBar,
+    required this.child,
     this.floatingActionButton,
     this.bottomNavigationBar,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BackgroundManager(
-      appBar: appBar,
-      body: child is Scaffold
-          ? child
-          : Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: appBar,
-              floatingActionButton: floatingActionButton,
-              bottomNavigationBar: bottomNavigationBar,
-              body: child,
-            ),
+    return Stack(
+      children: [
+        const BackgroundManager(), // background
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: appBar,
+          body: child,
+          floatingActionButton: floatingActionButton,
+          bottomNavigationBar: bottomNavigationBar,
+        ),
+      ],
     );
   }
 }
