984b0f74cb36e49dba0a7849d780812fa5813a0e
nancy0786 <the.anubhav786@gmail.com>
2025-06-19 00:23:12 +0530

    background_manager.dart

--- lib/widgets/background_manager.dart
+++ lib/widgets/background_manager.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
+import 'package:world_music_nancy/providers/preferences_provider.dart';
 
 class BackgroundManager extends StatefulWidget {
   const BackgroundManager({super.key});
@@ -10,33 +12,58 @@ class BackgroundManager extends StatefulWidget {
 }
 
 class _BackgroundManagerState extends State<BackgroundManager> {
-  late final String _selectedBackground;
+  late String _selectedBackground;
   VideoPlayerController? _videoController;
-
-  final List<String> _backgrounds = [
-    for (int i = 1; i <= 18; i++) 'assets/backgrounds/cyberpunk/cyber$i.mp4',
-    for (int i = 19; i <= 30; i++) 'assets/backgrounds/cyberpunk/cyber$i.jpg',
-    for (int i = 1; i <= 14; i++) 'assets/backgrounds/girls/girl$i.jpg',
-    for (int i = 15; i <= 30; i++) 'assets/backgrounds/girls/girl$i.mp4',
-    for (int i = 1; i <= 26; i++) 'assets/backgrounds/nature/nature$i.mp4',
-  ];
 
   @override
   void initState() {
     super.initState();
-    _selectedBackground = _backgrounds[Random().nextInt(_backgrounds.length)];
+    _initializeBackground();
+  }
 
-    if (_selectedBackground.endsWith('.mp4')) {
-      _videoController = VideoPlayerController.asset(_selectedBackground)
-        ..setLooping(true)
-        ..initialize().then((_) {
-          if (mounted) {
-            _videoController!.play();
-            setState(() {});
-          }
-        }).catchError((e) {
-          debugPrint('Video failed to load: $e');
-        });
+  void _initializeBackground() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final provider = Provider.of<PreferencesProvider>(context, listen: false);
+      final String category = provider.backgroundCategory;
+
+      final List<String> selectedList = _getBackgroundList(category);
+
+      _selectedBackground = selectedList[Random().nextInt(selectedList.length)];
+
+      if (_selectedBackground.endsWith('.mp4')) {
+        _videoController = VideoPlayerController.asset(_selectedBackground)
+          ..setLooping(true)
+          ..initialize().then((_) {
+            if (mounted) {
+              _videoController!.play();
+              setState(() {});
+            }
+          }).catchError((e) {
+            debugPrint('Video load error: $e');
+          });
+      } else {
+        setState(() {});
+      }
+    });
+  }
+
+  List<String> _getBackgroundList(String category) {
+    switch (category) {
+      case 'cyberpunk':
+        return [
+          for (int i = 1; i <= 18; i++) 'assets/backgrounds/cyberpunk/cyber$i.mp4',
+          for (int i = 19; i <= 30; i++) 'assets/backgrounds/cyberpunk/cyber$i.jpg',
+        ];
+      case 'nature':
+        return [
+          for (int i = 1; i <= 26; i++) 'assets/backgrounds/nature/nature$i.mp4',
+        ];
+      case 'girls':
+      default:
+        return [
+          for (int i = 1; i <= 14; i++) 'assets/backgrounds/girls/girl$i.jpg',
+          for (int i = 15; i <= 30; i++) 'assets/backgrounds/girls/girl$i.mp4',
+        ];
     }
   }
 
@@ -62,7 +89,7 @@ class _BackgroundManagerState extends State<BackgroundManager> {
                   fit: BoxFit.cover,
                   errorBuilder: (_, __, ___) => const ColoredBox(color: Colors.black),
                 ),
-          Container(color: Colors.black.withOpacity(0.4)), // dark overlay
+          Container(color: Colors.black.withOpacity(0.4)),
         ],
       ),
     );
