332993347931b26e38fe20f455340661ac0b92e9
nancy0786 <the.anubhav786@gmail.com>
2025-06-19 01:35:55 +0530

    neon_aware_container.dart

---
+++ lib/widgets/neon_aware_container.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:world_music_nancy/providers/preferences_provider.dart';
+
+class NeonAwareContainer extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final Color? color;
+
+  const NeonAwareContainer({
+    super.key,
+    required this.child,
+    this.padding,
+    this.margin,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isFuturistic = Provider.of<PreferencesProvider>(context).isFuturistic;
+
+    return Container(
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: color ?? (isFuturistic ? Colors.black.withOpacity(0.5) : Colors.grey.shade900),
+        borderRadius: BorderRadius.circular(16),
+        border: isFuturistic ? Border.all(color: Colors.cyanAccent, width: 1.5) : null,
+        boxShadow: isFuturistic
+            ? [
+                BoxShadow(
+                  color: Colors.cyanAccent.withOpacity(0.3),
+                  blurRadius: 10,
+                  spreadRadius: 1,
+                ),
+              ]
+            : [],
+      ),
+      child: child,
+    );
+  }
+}
