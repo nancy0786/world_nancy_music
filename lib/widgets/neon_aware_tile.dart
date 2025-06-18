f2f751e64cdbbe2ee7d3f626f61dea7ef89beec0
nancy0786 <the.anubhav786@gmail.com>
2025-06-19 01:38:18 +0530

    neon_aware_tile.dart

---
+++ lib/widgets/neon_aware_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:world_music_nancy/providers/preferences_provider.dart';
+
+class NeonAwareTile extends StatelessWidget {
+  final Widget title;
+  final Widget? subtitle;
+  final Widget? leading;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  const NeonAwareTile({
+    super.key,
+    required this.title,
+    this.subtitle,
+    this.leading,
+    this.trailing,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isFuturistic = Provider.of<PreferencesProvider>(context).isFuturistic;
+
+    return ListTile(
+      onTap: onTap,
+      leading: leading,
+      trailing: trailing,
+      title: DefaultTextStyle(
+        style: TextStyle(
+          color: isFuturistic ? Colors.cyanAccent : Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        child: title,
+      ),
+      subtitle: subtitle != null
+          ? DefaultTextStyle(
+              style: TextStyle(
+                color: isFuturistic ? Colors.white54 : Colors.white70,
+              ),
+              child: subtitle!,
+            )
+          : null,
+      tileColor: isFuturistic ? Colors.black54 : Colors.grey[850],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: isFuturistic
+            ? BorderSide(color: Colors.cyanAccent.withOpacity(0.5))
+            : BorderSide.none,
+      ),
+    );
+  }
+}
