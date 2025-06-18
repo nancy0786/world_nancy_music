03bc4a804c518e95d08f1899ae37e02a945d4235
nancy0786 <the.anubhav786@gmail.com>
2025-06-19 01:01:55 +0530

    neon_aware_button.dart

---
+++ lib/widgets/neon_aware_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:world_music_nancy/providers/preferences_provider.dart';
+import 'neon_button.dart';
+
+class NeonAwareButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  final IconData? icon;
+
+  const NeonAwareButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isFuturistic = Provider.of<PreferencesProvider>(context).isFuturistic;
+
+    return isFuturistic
+        ? NeonButton(text: text, icon: icon, onTap: onTap)
+        : ElevatedButton.icon(
+            onPressed: onTap,
+            icon: Icon(icon),
+            label: Text(text),
+          );
+  }
+}
