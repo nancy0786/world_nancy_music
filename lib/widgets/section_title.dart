626ebeac649fff9e579cb24cfd6f12ab069b7973
nancy0786 <the.anubhav786@gmail.com>
2025-06-18 05:49:30 +0530

    section_title.dart

--- lib/widgets/section_title.dart
+++ lib/widgets/section_title.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class SectionTitle extends StatelessWidget {
+  final String title;
+
+  const SectionTitle({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8, left: 4),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF00FFFF),
+          shadows: [
+            Shadow(color: Colors.pinkAccent, blurRadius: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
