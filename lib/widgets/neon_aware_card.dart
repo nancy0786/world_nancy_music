f009391e17b0f7e270287f449ecf756110799bee
nancy0786 <the.anubhav786@gmail.com>
2025-06-19 10:09:01 +0530

    neon_aware_card.dart

--- lib/widgets/neon_aware_card.dart
+++ lib/widgets/neon_aware_card.dart
@@ -1,4 +1,3 @@
-import 'package:world_music_nancy/widgets/neon_aware_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:world_music_nancy/providers/preferences_provider.dart';
@@ -7,24 +6,36 @@ class NeonAwareCard extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry? margin;
   final Color? color;
+  final ShapeBorder? shape;
+  final double? elevation;
+  final Color? shadowColor;
 
   const NeonAwareCard({
     super.key,
     required this.child,
     this.margin,
     this.color,
+    this.shape,
+    this.elevation,
+    this.shadowColor,
   });
 
   @override
   Widget build(BuildContext context) {
     final isFuturistic = Provider.of<PreferencesProvider>(context).isFuturistic;
 
-    return NeonAwareCard(
-      color: color ?? (isFuturistic ? Colors.black87 : Colors.grey.shade800),
+    return Card(
       margin: margin,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      elevation: isFuturistic ? 12 : 4,
-      shadowColor: isFuturistic ? Colors.cyanAccent.withOpacity(0.5) : Colors.black,
+      color: color ?? (isFuturistic ? Colors.black87 : Colors.grey.shade800),
+      shape: shape ??
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+            side: isFuturistic
+                ? BorderSide(color: Colors.cyanAccent.withOpacity(0.4), width: 1)
+                : BorderSide.none,
+          ),
+      elevation: elevation ?? (isFuturistic ? 12 : 4),
+      shadowColor: shadowColor ?? (isFuturistic ? Colors.cyanAccent.withOpacity(0.5) : Colors.black),
       child: child,
     );
   }
