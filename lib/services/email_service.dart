465eb78d945cba26e4ce41f653f3baf61d50a32d
nancy0786 <the.anubhav786@gmail.com>
2025-06-20 22:30:46 +0530

    email_service.dart

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class EmailService {
-  static const _serviceId = 'YOUR_SERVICE_ID';
-  static const _templateId = 'YOUR_TEMPLATE_ID';
-  static const _userId = 'YOUR_PUBLIC_KEY'; // Also called "User ID" or "Public Key"
+  static const _serviceId = 'your_service_id';
+  static const _templateId = 'your_template_id';
+  static const _userId = 'your_user_id';
 
   static Future<bool> sendLoginCode(String toEmail, String code) async {
     final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
