953a5554264bf51f7510e3165de69725815abd7e
nancy0786 <the.anubhav786@gmail.com>
2025-06-21 13:08:09 +0530

    youtube_autocomplete_service.dart

--- lib/services/youtube_autocomplete_service.dart
+++ lib/services/youtube_autocomplete_service.dart
@@ -4,14 +4,22 @@ import 'package:http/http.dart' as http;
 class YouTubeAutocompleteService {
   static Future<List<String>> fetchSuggestions(String query) async {
     try {
+      final encoded = Uri.encodeComponent(query);
       final url = Uri.parse(
-        'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=$query',
+        'https://clients1.google.com/complete/search?client=youtube&hl=en&gl=US&ds=yt&q=$encoded'
       );
-      final response = await http.get(url);
 
+      final response = await http.get(url);
       if (response.statusCode == 200) {
-        final body = json.decode(response.body);
-        return List<String>.from(body[1]);
+        final raw = response.body;
+
+        // Extract JSON string from raw response
+        final start = raw.indexOf('[');
+        final end = raw.lastIndexOf(']') + 1;
+        final jsonStr = raw.substring(start, end);
+        final List data = json.decode(jsonStr);
+
+        return List<String>.from(data[1].map((item) => item[0]));
       } else {
         return [];
       }
