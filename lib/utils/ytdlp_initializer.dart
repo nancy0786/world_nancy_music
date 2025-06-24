8409f7743d8f0993726fe738f3b40c50b24296a8
nancy0786 <the.anubhav786@gmail.com>
2025-06-24 10:44:44 +0530

    ytdlp_initializer.dart

--- lib/utils/ytdlp_initializer.dart
+++ lib/utils/ytdlp_initializer.dart
@@ -3,18 +3,22 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 class YtDlpInitializer {
-  static Future<String> initYtDlpBinary() async {
-    final dir = await getApplicationSupportDirectory();
-    final ytDlpPath = '${dir.path}/yt-dlp';
+  static Future<String> prepareYtDlp() async {
+    // Load yt-dlp binary from assets
+    final byteData = await rootBundle.load('assets/yt-dlp');
 
+    // Get temp directory to save the executable
+    final tempDir = await getTemporaryDirectory();
+    final ytDlpPath = '${tempDir.path}/yt-dlp';
     final ytDlpFile = File(ytDlpPath);
 
-    if (!await ytDlpFile.exists()) {
-      final byteData = await rootBundle.load('assets/yt-dlp');
-      await ytDlpFile.writeAsBytes(byteData.buffer.asUint8List());
-      await ytDlpFile.setExecutable(true);
-    }
+    // Write binary to temp file
+    await ytDlpFile.writeAsBytes(byteData.buffer.asUint8List());
 
-    return ytDlpPath;
+    // Make it executable using chmod
+    await Process.run('chmod', ['+x', ytDlpFile.path]);
+
+    // Return the path to be used for running
+    return ytDlpFile.path;
   }
 }
